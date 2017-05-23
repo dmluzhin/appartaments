b09bf37e2b01b10be4b8406ec809379386a461a4
Данила Лужин <d.m.luzhin@gmail.com>
2017-05-23 13:47:43 +0300

    гзв

--- kotelnicheskaya/js/icon_customImage.js
+++ kotelnicheskaya/js/icon_customImage.js
@@ -1,14 +1,14 @@
 ymaps.ready(function () {
 	var myMap = new ymaps.Map('map-1', {
-				center: [55.745656,37.642923],
+				center: [55.742042,37.646220],
 				zoom: 18
 			}, {
 				searchControlProvider: 'yandex#search'
 			}),
 
 			myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-				hintContent: 'Котельническая Набережная 13',
-				balloonContent: 'Котельническая Набережная 13'
+				hintContent: 'Котельническая Набережная 31',
+				balloonContent: 'Котельническая Набережная 31'
 			}, {
 				// Опции.
 				// Необходимо указать данный тип макета.
